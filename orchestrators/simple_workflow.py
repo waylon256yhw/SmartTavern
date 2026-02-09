b61e97634179f8256f6a072cc2c4cdd5162e9908
Hanwen Yu <waylon256yhw@gmail.com>
2026-02-10 01:44:21 +0800

    fix: use absolute import in simple_workflow to avoid relative import beyond top-level package

--- orchestrators/simple_workflow.py
+++ orchestrators/simple_workflow.py
@@ -10,7 +10,7 @@ from concurrent.futures import ThreadPoolExecutor
 from dataclasses import dataclass
 from typing import Any
 
-from ..core.api_registry import get_registry
+from core.api_registry import get_registry
 
 
 @dataclass
